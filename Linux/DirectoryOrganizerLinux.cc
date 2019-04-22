3fcdaa9b1dfc500effe9dc8c3c988664d496ba09
Matt Erickson <matterickson9819@gmail.com>
2019-04-22 18:07:22 -0500

    Added comments

--- Linux/DirectoryOrganizerLinux.cc
+++ Linux/DirectoryOrganizerLinux.cc
@@ -15,19 +15,24 @@ bool isDir(const char *file);
 int main(){
 
 	char name[50];
-       	getlogin_r(name, 50);
+	//getlogin_r returns the user name currently logged in, this is to navigate to the downloads folder	
+	getlogin_r(name, 50);
 
 	std::string strName = name; 
 	std::string path = "/home/" + strName + "/Downloads/";
 
+	//Runs is used to rename duplicate files, it gets incremented everytime a duplicate file is found
+	//runs then gets appended to the end of a duplicate file
 	int runs = 0;
 
 	traverse(path,runs);
+
 }//End of Main
 
 
 bool traverse(std::string path, int runs){
 
+	//initializes the vector to store files then clears it.
 	std::vector<std::string> files;
 	files.clear();
 	int place = 0;
@@ -35,11 +40,13 @@ bool traverse(std::string path, int runs){
 	
 	DIR *dp;
 	struct dirent * dirp;
+	//Opens up the downloads folder
 	if((dp = opendir(path.c_str())) == NULL){
 		std::cout << "Error opening " << path << std::endl;
 		return 1;
 	}
 
+	//Adds all the files to the vector
 	while (( dirp = readdir(dp)) !=NULL){
 		files.push_back(path + dirp->d_name);
 	}
@@ -47,7 +54,9 @@ bool traverse(std::string path, int runs){
 	closedir(dp);
 	
 	for(int i =0; i < files.size(); i++){
-		
+	
+		//Checks to see if the file is a directory or regular file,
+		//if it is a regular file it sends it to be renamed and moved
 		if(!isDir(files[i].c_str())){
 
 			sendFile(files[i],runs);
@@ -64,14 +73,19 @@ void sendFile(std::string path, int runs){
 
 	std::string dirPath = path.substr(0,path.find_last_of("/"));
 
+	//Grabs the extention of the file
 	std::string ext = path.substr(path.find_last_of(".")+1);
 
+	//This is so tar.gz files get sent to the tar/ directory
 	if(ext == "gz"){
 		ext = "tar";
 	}
 
+	//sets the string with the desired dest path
 	dirPath = dirPath + "/" + ext + '/';
 
+	//Checks to see if the destination path already exists or not, if it does not it creates the 
+	//desired directory
 	if(!isDir(dirPath.c_str())){
 	
 		std::string call = std::string("mkdir ") + dirPath;
@@ -82,6 +96,7 @@ void sendFile(std::string path, int runs){
 		}
 	}
 
+	//Grabs the file name with the extension
 	std::string fileName = path.substr(path.find_last_of("/")+1);
 	if(ext != "tar"){	
 		fileName = fileName.substr(0,fileName.find_last_of("."));
@@ -89,6 +104,7 @@ void sendFile(std::string path, int runs){
 
 	std::string destPath;
 
+	//if the extention is not tar.gz it adds the extension to the filename otherwise it does nothing
 	if(ext != "tar"){
 		 destPath = dirPath + fileName + "." + ext;
 		
@@ -97,6 +113,9 @@ void sendFile(std::string path, int runs){
 		destPath = dirPath + fileName;
 	}
 
+	//Checks to see if the file already exists in the destination directory, if it does 
+	//the file gets the run number appended before the extension. This is looped until the file does not exist
+	//in the destination directory, then the file is ready to be moved
 	while(access(destPath.c_str(), F_OK) != -1){
 		int lastIndex = fileName.find_last_of('.');
 		runs++;
@@ -105,6 +124,7 @@ void sendFile(std::string path, int runs){
 	}
 	std::cout << "Dest Path: " << destPath << std::endl;
 
+	//Moves the renamed file to its destination directory
 	if(rename(path.c_str(), destPath.c_str())){
 		std::cout << "Error moving files!" << std::endl;
 		exit(1);
@@ -112,6 +132,7 @@ void sendFile(std::string path, int runs){
 
 }
 
+//Checks to see if a file is a directory or regular file
 bool isDir(const char *file){
 	
 	DIR *directory = opendir(file);
