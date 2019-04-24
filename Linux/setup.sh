eaf93d31841b467b9abe9ca121cd493a3410dc6c
Matt Erickson <matterickson9819@gmail.com>
2019-04-23 19:17:05 -0500

    Script now creates directory for the program and copies the files to the new directory

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -2,3 +2,10 @@
 
 clear
 echo "Work in Progress..."
+
+echo "/home/$USER" 
+
+mkdir /home/$USER/Directory-Organizer
+
+cp DirectoryOrganizerLinux.cc /home/$USER/Directory-Organizer/
+cp setup.sh /home/$USER/Directory-Organizer/
