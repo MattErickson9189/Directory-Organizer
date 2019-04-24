ef2b1086ac39fff362d0f37804bdd2acbed2c489
Matt Erickson <matterickson9819@gmail.com>
2019-04-23 19:39:32 -0500

    Script now creates .desktop file

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -9,3 +9,13 @@ mkdir /home/$USER/Directory-Organizer
 
 cp DirectoryOrganizerLinux.cc /home/$USER/Directory-Organizer/
 cp setup.sh /home/$USER/Directory-Organizer/
+
+echo '#!/usr/bin/env xdg-open' >> Organizer.desktop
+echo '[Desktop Entry]' >> Organizer.desktop
+echo 'Version=1.0' >> Organizer.desktop
+echo 'Type=Application' >> Organizer.desktop
+echo 'Terminal=false' >> Organizer.desktop
+echo 'Exec='/home/$USER/Directory-Organizer/DirectoryOrganizerLinux.cc'' >> Organizer.desktop
+echo 'Name=keyMap' >> Organizer.desktop
+
+
