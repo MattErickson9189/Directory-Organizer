a3d4bb4816f7966d7ce9f68f826c435855676ebc
Matt Erickson <matterickson9819@gmail.com>
2019-04-30 11:51:27 -0500

    Config program reads from the csv file

--- Linux/config/config.cc
+++ Linux/config/config.cc
@@ -10,16 +10,38 @@ int main(){
 	
 	cout << "[1] View current configs\n" << "[2] Change sorting method\n" << "[3] Change directory to be sorted\n";
 	cout << "[4} Exit" << endl;
-		
+
+	string method;
+	string path;
+	int check = 0;
+
 	ifstream settings;
 	settings.open("settings.csv");
 
 	while(settings.good()){
 		string line;
 		getline(settings, line, ',');
-		cout << line << endl;
+		
+		if(line.length() > 1 && check == 0){
+				method = line;
+				check = 1;
+		}//end of check
+		else if(line.length() > 1 && check == 1){
+				path = line;
+				break;
+		}
+
 	}
 
 	settings.close();
+
+	int x;
+
+	cout << "Enter your choice: ";
+	cin >> x;
+
+	if(x == 1){
+		cout << "Method: " << method << endl << "Path: " << path << endl;
+	}
 
 }
