7201ca46583f6b1b6207b4c6b0759e3486829ffa
Matt Erickson <matterickson9819@gmail.com>
2020-04-23 12:44:12 -0500

    Grabs the correct username

--- Linux/main.cc
+++ Linux/main.cc
@@ -1,14 +1,26 @@
 #include "DirectoryOrganizer.h"
 #include "ExtensionMover.h"
+#include <unistd.h>
+
 
 int main(){
 
+    //On a unix system the max length a username can be is 32 characters long, just to add some extra room the buffer max will be size 50
+    int max = 50;
+
+    char *name = (char*)malloc(sizeof(*name) * max);
+
+    //getlogin_r returns the user name currently logged in, this is to navigate to the downloads folder    
+    getlogin_r(name, max);
+
+    std::string strName = name;
+    std::string path = "/home/" + strName + "/Downloads/";
 
     Traverser traverser;
     ExtensionMover mover;
     Organizer *organizer = &mover;
 
     DirectoryOrganizer directoryOrganizer(traverser,organizer);
-    directoryOrganizer.organize("/home/matt/Downloads/");
+    directoryOrganizer.organize(path);
 
 }
